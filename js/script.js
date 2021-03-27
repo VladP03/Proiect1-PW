7b36d472b8e6eadfd4d412eefa49b784b4bc8916
Vlad Paraschiv <florin-vladut.paraschiv@student.tuiasi.ro>
2021-03-27 21:08:17 +0200

    aproape am terminat sectiunea 2, mai am de verificat cate numere sunt match

--- js/script.js
+++ js/script.js
@@ -1,18 +1,13 @@
-/*
-var x; // document.getElementById("p1");
-
-function laIncarcare() {
-    x = document.getElementById("p1");
-    x.innerHTML="Eduard";
-    x.style.backgroundColor="red";
-}
-*/
-
 function laIncarcare() {
     getURL();
     getNavigator();
     getDate();
+    submitForm();
 }
+
+/*
+    Sectiunea 1
+*/
 
 // refresh local date 
 var timerDate = setInterval(getDate, 1000);
@@ -34,12 +29,8 @@ function getNavigator() {
     document.getElementById("showOperatingSystem").innerHTML = "Operating system= " + navigator.platform;
 
     // current location
-    getLocation();
+    //getLocation();
 }
-
-// get current location
-var currentLatitude;
-var currentLongitude;
 
 function getLocation() {
     if (navigator.geolocation) {
@@ -50,14 +41,70 @@ function getLocation() {
 }
 
 function getCoordinates(position) {
-    currentLatitude = position.coords.latitude;
-    currentLongitude = position.coords.longitude;
+    var currentLatitude = position.coords.latitude;
+    var currentLongitude = position.coords.longitude;
     
     document.getElementById("showLocation").innerHTML = "Location: Latitude= " + currentLatitude + " Longitude= " + currentLongitude;
-
-    // now latitude and longitude are available
-    //alert(currentLongitude+" and "+currentLatitude);
 }
 
-// ********************* sectiunea 2
+
+/*
+    Sectiunea 2
+*/
+var arrayOfLotoNumbers = {}
+
+function displayLotoNumbers() {
+    var x = document.getElementById("displayLotoNumbers");
+
+    let i;
+    for (i=0;i<8;i++) {
+        arrayOfLotoNumbers[i] = [(Math.floor(Math.random() * 255)).toString(16).toUpperCase()];
+    }
+
+    x.innerHTML="Numerele extrase sunt: ";
+    for (i=0;i<8;i++) {
+        if (i == 7) {
+            x.innerHTML += arrayOfLotoNumbers[i] + ".";
+        } else {
+            x.innerHTML += arrayOfLotoNumbers[i] + ", ";
+        }
+    }
+}
+
+function displayHowManyNumbersMatch() {
+    var x = document.getElementById("displayHowManyNumbersMatch");
+    x.innerHTML="Numere nimerite: 0";
+
+    let i;
+    let j;
+    let counter=0;
+    for (i=0;i<8;i++) {
+        let temp = document.getElementById(i);
+        for (j=0;j<8;j++) {
+            console.log(Nuber(temp))
+            if (Number(temp) == arrayOfLotoNumbers[i]) {
+                counter++;
+            }
+        }
+    }
+
+    console.log(counter);
+
+    x.innerHTML="Numere nimerite: " + counter;
+}
+
+function submitForm() {
+    var data = new FormData();
+    data.append("KEY", document.getElementById("FIELD"));
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "SERVER-SCRIPT");
+    xhr.send(data); 
+}
+
+
+
+
+/*
+    Sectiunea 3
+*/
 
