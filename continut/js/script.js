42487c27133ce9e87a75d1239cb7a1f94f122ef5
Vlad Paraschiv <florin-vladut.paraschiv@student.tuiasi.ro>
2021-04-20 13:34:49 +0300

    cateva modificari

--- continut/js/script.js
+++ continut/js/script.js
@@ -12,7 +12,7 @@ function laIncarcare() {
 */
 
 // refresh local date 
-var timerDate = setInterval(getDate, 1000);
+setInterval(getDate, 1000);
 function getDate() {
     if (document.getElementById("showDate") != null) {
         document.getElementById("showDate").innerHTML = "Date= " + new Date().toLocaleString();
@@ -21,16 +21,22 @@ function getDate() {
 
 // show URL
 function getURL() {
-    document.getElementById("showURL").innerHTML = "URL= " + window.location;
+    if (document.getElementById("showURL") != null) {
+        document.getElementById("showURL").innerHTML = "URL= " + window.location;
+    }
 }
 
 // show Navigator
 function getNavigator() {
     // browser name
-    document.getElementById("showBrowser").innerHTML = "Browser= " + navigator.userAgent;
+    if (document.getElementById("showBrowser") != null) {
+        document.getElementById("showBrowser").innerHTML = "Browser= " + navigator.userAgent;
+    }
 
     // operating system
-    document.getElementById("showOperatingSystem").innerHTML = "Operating system= " + navigator.platform;
+    if (document.getElementById("showOperatingSystem") != null) {
+        document.getElementById("showOperatingSystem").innerHTML = "Operating system= " + navigator.platform;
+    }
 
     // current location
     getLocation();
@@ -48,7 +54,9 @@ function getCoordinates(position) {
     var currentLatitude = position.coords.latitude;
     var currentLongitude = position.coords.longitude;
     
-    document.getElementById("showLocation").innerHTML = "Location: Latitude= " + currentLatitude + " Longitude= " + currentLongitude;
+    if (document.getElementById("showLocation") != null) {
+        document.getElementById("showLocation").innerHTML = "Location: Latitude= " + currentLatitude + " Longitude= " + currentLongitude;
+    }
 }
 
 
@@ -57,27 +65,36 @@ function getCoordinates(position) {
 /*
     Sectiunea 2
 */
-var arrayOfLotoNumbers = []
-var displayLotoNumbersElement = document.getElementById("displayLotoNumbers");
-var displayHowManyNumbersMatchElement = document.getElementById("displayHowManyNumbersMatch");
 
 function displayLotoNumbers() {
+    var arrayOfLotoNumbers = []
+    var displayLotoNumbersElement = document.getElementById("displayLotoNumbers");
+    var displayHowManyNumbersMatchElement = document.getElementById("displayHowManyNumbersMatch");
+
     let i;
     for (i=0;i<8;i++) {
         arrayOfLotoNumbers[i] = (Math.floor(Math.random() * 255)).toString(16).padStart(2, "0").toUpperCase();
     }
 
-    displayLotoNumbersElement.innerHTML ="Numerele extrase sunt: ";
-    for (i=0;i<8;i++) {
-        if (i == 7) {
-            displayLotoNumbersElement.innerHTML += arrayOfLotoNumbers[i] + ".";
-        } else {
-            displayLotoNumbersElement.innerHTML += arrayOfLotoNumbers[i] + ", ";
+    if (displayLotoNumbersElement != null) {
+        displayLotoNumbersElement.innerHTML ="Numerele extrase sunt: ";
+
+        for (i=0;i<8;i++) {
+            if (i == 7) {
+                displayLotoNumbersElement.innerHTML += arrayOfLotoNumbers[i] + ".";
+            } else {
+                displayLotoNumbersElement.innerHTML += arrayOfLotoNumbers[i] + ", ";
+            }
         }
     }
+
+    displayHowManyNumbersMatch(arrayOfLotoNumbers);
 }
 
-function displayHowManyNumbersMatch() {
+function displayHowManyNumbersMatch(arrayOfLotoNumbers) {
+    var displayLotoNumbersElement = document.getElementById("displayLotoNumbers");
+    var displayHowManyNumbersMatchElement = document.getElementById("displayHowManyNumbersMatch");
+
     let i;
     let j;
     let temp;   // punem valoarea din input dat de utilizator
@@ -86,18 +103,18 @@ function displayHowManyNumbersMatch() {
     for (i=0;i<8;i++) {
         temp = document.getElementById((i+1) + "-input");
         let tempHex = temp.value.toString(16).toUpperCase();
-        console.log(temp);
         for (j=0;j<8;j++) {
             if (tempHex == arrayOfLotoNumbers[j]) {
                 counter++;
-                console.log(arrayOfLotoNumbers[j]);
                 arrayOfLotoNumbers[j] = "change value";
                 break;
             }
         }
     }
 
-    displayHowManyNumbersMatchElement.innerHTML= "Numere nimerite: " + counter;
+    if (displayHowManyNumbersMatchElement != null) {
+        displayHowManyNumbersMatchElement.innerHTML= "Numere nimerite: " + counter;
+    }
 }
 
 
